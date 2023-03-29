b1dff41cf482376c1b82bef8dd5910bc08ece1d1
andriideikalo <wlada801@gmail.com>
2023-03-29 14:42:04 +0300

    fix

---
+++ contacts.js
@@ -0,0 +1,20 @@
+const path = require("path");
+
+const contactsPath = path.join("./db/contacts.json");
+
+// TODO: задокументировать каждую функцию
+function listContacts() {
+  // ...твой код
+}
+
+function getContactById(contactId) {
+  // ...твой код
+}
+
+function removeContact(contactId) {
+  // ...твой код
+}
+
+function addContact(name, email, phone) {
+  // ...твой код
+}
