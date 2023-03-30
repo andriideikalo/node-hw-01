26057650c88fd786c59667e4d4a34eb56e188ed8
andriideikalo <wlada801@gmail.com>
2023-03-30 13:41:42 +0300

    fix

--- contacts.js
+++ contacts.js
@@ -1,5 +1,13 @@
 const path = require("path");
 
+fs.readFile("readme.txt", data)
+  .then((data) => console.log(data.toString()))
+  .catch((err) => console.log(err.message));
+
+fs.writeFile("readme.txt", data)
+  .then((data) => console.log(data.toString()))
+  .catch((err) => console.log(err.message));
+
 const contactsPath = path.join("./db/contacts.json");
 
 // TODO: задокументировать каждую функцию
@@ -18,3 +26,9 @@ function removeContact(contactId) {
 function addContact(name, email, phone) {
   // ...твой код
 }
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
